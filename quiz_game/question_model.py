b7ba01429a975763bcefa8ac32ac282d95e1e1f5
shaialoni <alonishai@gmail.com>
2023-01-26 18:15:55 -0800

    day 17, started quiz game

---
+++ quiz_game/question_model.py
@@ -0,0 +1,12 @@
+from data import question_data
+
+class Question():
+
+    def __str__(self) -> str:
+        return f"The text is {self.text} and the answer is {self.answer}"
+
+
+    def __init__(self, text, answer) -> None:
+        self.text = text
+        self.answer = answer
+
---
+++ quiz_game/quiz_brain.py
@@ -0,0 +1,8 @@
+class QuizBrain():
+
+    def __init__(self, question_list) -> None:
+        self.question_number = 0
+        self.question_list = question_list
+
+
+    def next_question():
