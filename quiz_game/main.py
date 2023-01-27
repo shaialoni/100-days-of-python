ab7a1181638d0a57254aebcee0e883afb95159ed
shaialoni <alonishai@gmail.com>
2023-01-26 23:28:05 -0800

    game finished, questions updated

--- quiz_game/main.py
+++ quiz_game/main.py
@@ -5,8 +5,8 @@ from quiz_brain import QuizBrain
 question_bank = []
 
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
@@ -16,3 +16,5 @@ quiz = QuizBrain(question_bank)
 while quiz.still_has_questions():
     quiz.next_question()
 
+print("You completed the quiz!")
+print(f"Your final score is {quiz.score}/{quiz.question_number}")
