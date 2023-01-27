b8cb010391cd4f4b615601422afdb82ae6e48491
shaialoni <alonishai@gmail.com>
2023-01-26 22:27:58 -0800

    created classes and quiz engine

--- quiz_game/question_model.py
+++ quiz_game/question_model.py
@@ -2,8 +2,8 @@ from data import question_data
 
 class Question():
 
-    def __str__(self) -> str:
-        return f"The text is {self.text} and the answer is {self.answer}"
+    # def __str__(self) -> str:
+    #     return f"The text is {self.text} and the answer is {self.answer}"
 
 
     def __init__(self, text, answer) -> None:
--- quiz_game/quiz_brain.py
+++ quiz_game/quiz_brain.py
@@ -4,5 +4,17 @@ class QuizBrain():
         self.question_number = 0
         self.question_list = question_list
 
+    def still_has_questions(self):
+        #print("len List", len(self.question_list))
+        if len(self.question_list) > self.question_number:
+            return True
+        else:
+            return False
 
-    def next_question():
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+
+    
