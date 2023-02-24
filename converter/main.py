9c1a7d24d5d42bc17ca3f42717fb2d4d7bde41b5
shaialoni <alonishai@gmail.com>
2023-02-24 14:36:24 -0800

    Day 30, refactor NATO alphabet app, refactor password manager app

--- converter/main.py
+++ converter/main.py
@@ -1,41 +1,29 @@
 from tkinter import *
 
+def convert():
+    user_input = float(user_entry.get())
+    converted_input = round((user_input * 1.609), 2)
+    #print(converted_input)
+    label_result_num.config(text=f"{converted_input}")
+
 window = Tk()
 window.title("Unit converter")
-window.minsize(width=250, height=150)
 window.config(padx=25, pady=25)
 
-# Labels - unit1, unit2, descriptions
 label_unit1 = Label(text="Miles")
 label_unit2 = Label(text="Km")
-label_result_num = Label()
+label_result_num = Label(text="0")
 label_result_text = Label(text="Is equal to")
+
 label_unit1.grid(column=2, row=0)
 label_unit2.grid(column=2, row=1)
 label_result_num.grid(column=1, row=1)
 label_result_text.grid(column=0, row=1)
 
-# User input
-entry = Entry(width=5)
-entry.grid(column=1, row=0)
+user_entry = Entry(width=5)
+user_entry.grid(column=1, row=0)
 
-# Calculate button
-
-# create calculation function
-def convert():
-    user_input = float(entry.get())
-    converted_input = round((user_input * 1.6), 2)
-    print(converted_input)
-    label_result_num.config(text=f"{converted_input}")
-
-
-button = Button(text="Calculate", command=convert)
+button = Button(text="Click to calculate", command=convert)
 button.grid(column=1, row=3)
-
-
-
-
-
-
 
 window.mainloop()
--- password_manager/main.py
+++ password_manager/main.py
@@ -2,6 +2,8 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
@@ -20,26 +22,46 @@ def generate_password():
     password = ''.join(password)
     password_entry.insert(0, password)
     pyperclip.copy(password)
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
 
     website = input_website.get()
     username = input_username.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": username,
+            "password": password
+        }
+    }
+
+    def write_to_file(data_to_file):
+        json.dump(data_to_file, data_file, indent=4)
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please fill in all the fields")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {username} \nPassword: {password} \n Is it ok to save?")
-
-        if is_ok:
-            with open("data.txt", "a") as data:
-                data.write(f"{website} | {username} | {password}\n")
-
+        try:
+            with open("data.json", "r") as data_file:
+                # reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                write_to_file(new_data)
+        else:
+            # updating old data with new data
+            data.update(new_data)
+            with open("data.json", "w") as data_file:
+                # saving updated data
+                write_to_file(data)
+        finally:
             input_website.delete(0, "end")
             password_entry.delete(0, "end")
-# ---------------------------- UI SETUP ------------------------------- #
 
+
+# ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
@@ -49,6 +71,7 @@ lock_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=lock_img)
 canvas.grid(row=0, column=1)
 
+# Labels
 label_website = Label(text="Website: ", font=("Arial", 14))
 label_website.grid(row=1, column=0)
 label_username = Label(text="Email / Username: ", font=("Arial", 14))
@@ -56,6 +79,7 @@ label_username.grid(row=2, column=0)
 label_password = Label(text="Password", font=("Arial", 14))
 label_password.grid(row=3, column=0)
 
+# Entries
 input_website = Entry(width=35)
 input_website.grid(row=1, column=1, columnspan=2)
 input_website.focus()
@@ -65,10 +89,11 @@ input_username.grid(row=2, column=1, columnspan=2)
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
+# Buttons
 generate_button = Button(text="Generate", command=generate_password)
 generate_button.grid(row=3, column=2)
-
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
+
 
 window.mainloop()
